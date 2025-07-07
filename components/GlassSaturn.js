011a412a46b00ba6cb3684cefded512460852925
kaliwoolf <kunnskap@pm.me>
2025-07-08 02:26:50 +0300

    .

--- components/GlassSaturn.js
+++ components/GlassSaturn.js
@@ -1,55 +1,40 @@
-import React, { useRef, useEffect } from 'react'
+// components/GlassSaturn.js
+import React, { useRef } from 'react'
 import { useFrame } from '@react-three/fiber'
-import { MeshTransmissionMaterial, Environment } from '@react-three/drei'
-import * as THREE from 'three'
+import { MeshPhysicalMaterial } from 'three'
+import { useGLTF } from '@react-three/drei'
 
-export default function GlassSaturn({ mouse }) {
+export default function GlassSaturn(props) {
   const saturnRef = useRef()
-  const ringsRef = useRef()
 
-  // Покачивание при движении мыши
-  useFrame(() => {
+  useFrame(({ clock }) => {
+    const t = clock.getElapsedTime()
     if (saturnRef.current) {
-      saturnRef.current.rotation.y += 0.002
-      saturnRef.current.rotation.x += (mouse.current.y - saturnRef.current.rotation.x) * 0.02
-      saturnRef.current.rotation.z += (mouse.current.x - saturnRef.current.rotation.z) * 0.02
-    }
-
-    if (ringsRef.current) {
-      ringsRef.current.rotation.z = 0.4 // Наклон колец
+      saturnRef.current.rotation.y = t * 0.05
     }
   })
 
   return (
-    <group position={[1.2, 0.6, 0]}>
-      {/* Сам Сатурн — кристаллический */}
-      <mesh ref={saturnRef}>
-        <icosahedronGeometry args={[0.5, 2]} />
-        <MeshTransmissionMaterial
-          backside
-          thickness={0.6}
-          roughness={0}
+    <group
+      ref={saturnRef}
+      position={[2, 1.5, 0]}
+      rotation={[Math.PI / 6, Math.PI / 4, 0]} // наклон оси
+      scale={[2.5, 2.5, 2.5]} // увеличенный размер
+    >
+      <mesh>
+        <sphereGeometry args={[1, 64, 64]} />
+        <meshPhysicalMaterial
           transmission={1}
-          ior={1.5}
-          chromaticAberration={0.06}
-          anisotropy={0.1}
-          distortion={0.4}
-          distortionScale={0.5}
-          temporalDistortion={0.2}
+          roughness={0}
+          thickness={0.5}
           clearcoat={1}
-          attenuationColor="#aaffff"
-          attenuationDistance={1.2}
+          clearcoatRoughness={0}
+          reflectivity={1}
+          ior={1.5}
+          color="#aaccff"
+          side={2}
         />
       </mesh>
-
-      {/* Кольца */}
-      <mesh ref={ringsRef} rotation={[Math.PI / 2, 0, 0]}>
-        <ringGeometry args={[0.7, 1.1, 64]} />
-        <meshBasicMaterial color="#8899ff" transparent opacity={0.15} side={THREE.DoubleSide} />
-      </mesh>
-
-      {/* Окружение для преломлений */}
-      <Environment preset="sunset" background={false} />
     </group>
   )
 }
