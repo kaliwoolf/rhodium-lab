308e7915ea371bb36eef86a6131a129e7ee0f7d2
kaliwoolf <kunnskap@pm.me>
2025-07-08 00:36:24 +0300

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,10 @@
 import '../styles/globals.css'
-// import BackgroundEffect from '../components/BackgroundEffect'
-// import ShaderBackground from '../components/ShaderBackground'
-import Anomaly from '../components/Anomaly'
+import ShaderBackground from '../components/ShaderBackground'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      {/* <BackgroundEffect /> */}
-      {/* <ShaderBackground /> */}
-      <Anomaly />
+      <ShaderBackground />
       <Component {...pageProps} />
     </>
   )
