369f0217d8f15274bcc4090a69645423d0798b22
kaliwoolf <kunnskap@pm.me>
2025-07-09 01:00:59 +0300

    .

--- components/GlassSaturn.js
+++ components/GlassSaturn.js
@@ -62,7 +62,7 @@ export default function GlassSaturn() {
       {/* Объёмные кольца (торы) */}
       <group position={[0, 0.1, 0]} rotation={[Math.PI / 2.2, 0, 0]} renderOrder={4} ref={ringRef}>
         <mesh>
-          <torusGeometry args={[0.88, 0.012, 64, 256]} />
+          <torusGeometry args={[1.88, 0.012, 64, 256]} />
           <meshPhysicalMaterial
             transmission={1}
             thickness={0.55}                  
