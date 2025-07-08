74acd5ce01659c9f7e195b0568817e2d2dd2831a
kaliwoolf <kunnskap@pm.me>
2025-07-08 23:19:32 +0300

    .

--- components/ThreeBackground.js
+++ components/ThreeBackground.js
@@ -147,12 +147,13 @@ export default function ThreeBackground() {
       <ambientLight intensity={0.5} />
       <pointLight position={[5, 5, 5]} intensity={3} color="#1d1f22" />
       
-      <Starfield mouse={mouse} />
+      
       <mesh position={[2.5, 1.6, -10]}>
         <planeGeometry args={[100, 100]} />
         <meshStandardMaterial color="#1a1c20" roughness={1} metalness={0} />
       </mesh>
       <GlassSaturn mouse={mouse} />
+      <Starfield mouse={mouse} />
       
 
       <EffectComposer>
