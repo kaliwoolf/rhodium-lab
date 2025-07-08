2d370ee70592e3e525df547acc0302e1a43cc014
kaliwoolf <kunnskap@pm.me>
2025-07-08 22:43:14 +0300

    .

--- components/GlassSaturn.js
+++ components/GlassSaturn.js
@@ -55,7 +55,7 @@ export default function GlassSaturn() {
             envMapIntensity={1.2}
             color="#ffffff"
             attenuationDistance={0.5}
-            attenuationColor="#88ffaa"
+            attenuationColor="#ffffff"
           />
       </mesh>
 
@@ -114,7 +114,7 @@ export default function GlassSaturn() {
         </group>
 
       {/* Свет */}
-      <Environment background={false}>
+      <Environment preset="studio" background={false}>
         <Lightformer intensity={2.5} position={[6, 4, -4]} scale={[5, 5, 1]} color="#445566" />
         <Lightformer intensity={3} position={[0, 0, 5]} scale={[4, 4, 1]} color="#ffffff" />
       </Environment>
