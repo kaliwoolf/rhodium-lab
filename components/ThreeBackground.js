f2afc52ab738fb00f772b12d3d4c7ef9aed8442c
kaliwoolf <kunnskap@pm.me>
2025-07-09 21:39:06 +0300

    .

--- components/ThreeBackground.js
+++ components/ThreeBackground.js
@@ -152,7 +152,7 @@ export default function ThreeBackground() {
     >
       <Suspense fallback={null}>
 
-        <group position={[-1.5, -1.6, 0]} scale={[1.8, 1.8, 1.8]}>
+        <group position={[-2, -1.8, 0]} scale={[1.8, 1.8, 1.8]}>
           <GlassSaturn mouse={mouse} />
         </group>
 
