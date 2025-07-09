ed598861ec406b9a7e6e2462b4829d783718cbbe
kaliwoolf <kunnskap@pm.me>
2025-07-09 23:57:24 +0300

    .

--- pages/index.js
+++ pages/index.js
@@ -17,13 +17,24 @@ export default function Home() {
         <HeroSection />
 
       <motion.section
-        initial={{ opacity: 0, y: 50 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1 }}
-        className="h-screen bg-black text-white flex items-center justify-center"
-      >
-        <p className="text-4xl">А вот и второй экран!</p>
-      </motion.section>
+          id="projects"
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          className="h-screen bg-black text-white flex items-center justify-center"
+        >
+          <p className="text-4xl">Проекты будут здесь ✨</p>
+        </motion.section>
+
+        <motion.section
+          id="contact"
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          className="h-screen bg-[#111] text-white flex items-center justify-center"
+        >
+          <p className="text-4xl">Контакты</p>
+        </motion.section>
     </main>
     </>
   )
