5eba68ff833a41d7198a2651742e56f6083f96ec
kaliwoolf <kunnskap@pm.me>
2025-07-10 21:29:04 +0300

    .

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -79,7 +79,7 @@ export default function HeroSection() {
               viewBox="0 0 54 10"
               width="54"
               height="10"
-              className="absolute top-[35%] left-1/2 -translate-x-1/2 -translate-y-1/2"
+              className="absolute top-[46%] left-[calc(50%-27px)]"
               animate={{
                 x: [0, 0.4, -0.3, 0.2, -0.2, 0],
                 y: [0, -0.2, 0.3, -0.1, 0],
