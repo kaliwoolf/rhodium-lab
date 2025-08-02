6514a0a5f31c4985557336d69253334da7638337
kaliwoolf <kunnskap@pm.me>
2025-08-02 17:32:27 +0300

    .

--- components/GlassVideoEffect.js
+++ components/GlassVideoEffect.js
@@ -1,13 +1,30 @@
 // components/GlassVideoEffect.js
+import { useRef } from 'react'
 import styles from '../styles/GlassVideoEffect.module.css'
 
 export default function GlassVideoEffect({ src, className = '' }) {
+ 
+  const videoRef = useRef(null)
+
+  const handleMouseEnter = () => {
+    videoRef.current?.play()
+  }
+
+  const handleMouseLeave = () => {
+    videoRef.current?.pause()
+    videoRef.current.currentTime = 0
+  }
+
+
   return (
-    <div className={`${styles.wrapper} ${className}`}>
+    <div className={`${styles.wrapper} ${className}`}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+    >
       <div className={styles.videoContainer}>
         <video
+          ref={videoRef} 
           src={src}
-          autoPlay
           muted
           loop
           playsInline
