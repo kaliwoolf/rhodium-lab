f50bec1b4d6865d919b9ce72b0d25e54a1f12ecf
kaliwoolf <kunnskap@pm.me>
2025-08-03 13:18:05 +0300

    Мерж обновлений с репозитория

--- components/ScrambleLinkMenu.js
+++ components/ScrambleLinkMenu.js
@@ -1,19 +1,12 @@
 import ScrambleLink from '../components/ScrambleLink'
-import ScrambleExternalLink from '../components/ScrambleExternalLink'
 
 export default function ScrambleLinkMenu() {
   return (
     <div className="absolute top-8 right-12 z-50 flex gap-10 text-white text-lg font-semibold tracking-wide">
-<<<<<<< HEAD
-      <ScrambleLink text="Проекты" onClick={() => scrollToId('projects')} />
-      <ScrambleLink text="Связаться" onClick={() => scrollToId('contact')} />
-      <ScrambleExternalLink text="Кабинет" href="https://mysteriumlab.pro/teach/control" />
-=======
       <ScrambleLink
         text="Кабинет"
         href="https://mysteriumlab.pro/teach/control"
       />
->>>>>>> 5bcca47f72b890c3c7dfb16d00ed6b4e8b510ef5
     </div>
   )
 }
