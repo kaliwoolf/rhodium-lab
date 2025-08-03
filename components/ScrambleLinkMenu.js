3406593ffcd029d02cc599570c156e1bc53b3f97
kaliwoolf <164188674+kaliwoolf@users.noreply.github.com>
2025-08-03 02:19:54 +0100

    Update ScrambleLinkMenu.js

--- components/ScrambleLinkMenu.js
+++ components/ScrambleLinkMenu.js
@@ -8,14 +8,7 @@ export default function ScrambleLinkMenu() {
 
   return (
     <div className="absolute top-8 right-12 z-50 flex gap-10 text-white text-lg font-semibold tracking-wide">
-      <ScrambleLink
-          text="Кабинет"
-          onClick={() => {
-            setTimeout(() => {
-                window.open('https://mysteriumlab.pro/teach/control', '_blank')
-              }, 1000)
-            }}
-        />
+      <ScrambleLink text="Кабинет" href="https://mysteriumlab.pro/teach/control" />
     </div>
   )
 }
