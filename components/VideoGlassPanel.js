6220823a248631442a515748f992f47ee834211d
kaliwoolf <kunnskap@pm.me>
2025-08-06 01:10:55 +0300

    .

--- components/VideoGlassPanel.js
+++ components/VideoGlassPanel.js
@@ -39,33 +39,35 @@ function GlassPanel({ videoUrl }) {
     }
   }, [videoUrl])
 
+  // Панель чуть наклонена для 3D-эффекта!
   return (
     <>
       {/* Видео-слой (фон) */}
       {videoTexture && (
-        <mesh position={[0, 0, -0.16]} scale={[1.27, 0.71, 1]}>
+        <mesh position={[0, 0, -0.18]} scale={[1.45, 0.85, 1]}>
           <planeGeometry args={[1.4, 0.8]} />
           <meshBasicMaterial map={videoTexture} toneMapped={false} />
         </mesh>
       )}
       {/* Стеклянная панель поверх */}
-      <mesh ref={mesh}>
+      <mesh ref={mesh} rotation={[0.23, -0.32, 0]}>
         <planeGeometry args={[1.25, 0.75]} />
         <meshPhysicalMaterial
           transmission={1}
-          transparent
-          thickness={0.27}
-          ior={1.51}
-          roughness={0.01}
-          metalness={0.04}
-          reflectivity={1}
-          attenuationColor="#e6e6ff"
-          attenuationDistance={1.7}
-          color="#ffffff"
-          clearcoat={0.9}
-          clearcoatRoughness={0.15}
-          iridescence={0.18}
-          envMapIntensity={1.5}
+          thickness={0.78}
+          ior={1.475}
+          roughness={0.04}
+          metalness={0.09}
+          reflectivity={0.98}
+          clearcoat={0.88}
+          clearcoatRoughness={0.14}
+          envMapIntensity={2.1}
+          color="#d0e7ff"
+          iridescence={0.13}
+          iridescenceIOR={1.12}
+          iridescenceThicknessRange={[180, 520]}
+          attenuationColor="#b8e8ff"
+          attenuationDistance={0.47}
         />
       </mesh>
     </>
@@ -76,18 +78,20 @@ export default function VideoGlassPanel({ videoUrl = "/video/00002.mp4" }) {
   return (
     <div style={{ width: "100vw", height: "100vh", background: "#171923" }}>
       <Canvas
-        camera={{ position: [0, 0, 2.5], fov: 40 }}
+        camera={{ position: [0, 0, 2.7], fov: 40 }}
         gl={{ antialias: true, alpha: true, preserveDrawingBuffer: false }}
       >
-        <ambientLight intensity={0.32} />
-        <directionalLight position={[3, 2, 3]} intensity={0.66} />
+        {/* Свет поярче для бликов */}
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[3, 2, 3]} intensity={1.16} />
         <GlassPanel videoUrl={videoUrl} />
+        {/* Окружение даёт классные reflections */}
         <Environment preset="sunset" />
         <OrbitControls
           enablePan={false}
           enableZoom={false}
-          maxPolarAngle={Math.PI / 2.15}
-          minPolarAngle={Math.PI / 2.3}
+          maxPolarAngle={Math.PI / 2.12}
+          minPolarAngle={Math.PI / 2.6}
         />
       </Canvas>
     </div>
