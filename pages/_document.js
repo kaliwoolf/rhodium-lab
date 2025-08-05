0cb0ac23be554b8f11b0442627594db615c7498c
kaliwoolf <kunnskap@pm.me>
2025-08-05 16:27:09 +0300

    .

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
       </Head>
       <body>
         <Main />
