fa17c0302ec0aabd8a6645ae1a78a0e487f1ed19
kaliwoolf <kunnskap@pm.me>
2025-08-06 13:48:41 +0300

    .

--- components/VideoGlassPanel.js
+++ components/VideoGlassPanel.js
@@ -119,7 +119,7 @@ function GlassPanel({ videoUrl }) {
       <primitive
         ref={mesh}
         object={nodes.Panel} // или nodes.Panel, если так назвал в Blender
-        scale={[0.3, 0.3, 1]} // ← подбери значения под себя!
+        scale={[0.6, 0.3, 0.04]}
         rotation={[0, 0, 0]}
         onPointerMove={handlePointerMove}
         onPointerOut={handlePointerOut}
