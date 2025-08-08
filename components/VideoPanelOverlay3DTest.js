903b5ac699178f452024b0bab16d1865444bb7d4
kaliwoolf <kunnskap@pm.me>
2025-08-08 21:26:37 +0300

    .

--- components/VideoPanelOverlay3DTest.js
+++ components/VideoPanelOverlay3DTest.js
@@ -292,7 +292,7 @@ export default function VideoPanelOverlay3DTest() {
   return (
     <div style={{ width: "100vw", height: "100vh", background: "#171923" }}>
       <Canvas
-        camera={{ position: [0, 0, 4], fov: 75 }}
+        camera={{ position: [0, 0, 8], fov: 25 }}
         gl={{ antialias: true, alpha: true, preserveDrawingBuffer: false }}
       >
         <ambientLight intensity={2.8} />
