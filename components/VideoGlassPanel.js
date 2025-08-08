70a9b75a60e43aec20f50283b34d58ab0aca1e70
kaliwoolf <kunnskap@pm.me>
2025-08-08 20:40:04 +0300

    .

--- components/VideoGlassPanel.js
+++ components/VideoGlassPanel.js
@@ -65,7 +65,7 @@ function GlassPanel({ videoUrl }) {
   const [videoTexture, setVideoTexture] = useState(null)
   const [hovered, setHovered] = useState(false)
   const [mouse, setMouse] = useState({ x: 0, y: 0 })
-  const { nodes } = useGLTF('/models/p1.glb')
+  const { nodes } = useGLTF('/models/p2.glb')
   console.log('nodes:', nodes)
 
 
